3e7072d46ea1575574040c2c273f96d4f2f89cc2
cm2025ma-cmyk <cholinmatthieu@gmail.com>
2026-01-07 15:25:47 +0100

    Update __init__.py
    fonction nom

--- __init__.py
+++ __init__.py
@@ -39,15 +39,26 @@ def authentification():
 
     return render_template('formulaire_authentification.html', error=False)
 
-@app.route('/fiche_client/<int:post_id>')
-def Readfiche(post_id):
+@app.route('/fiche_client/<string:nom>')
+def Readfiche(nom):
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM clients WHERE id = ?', (post_id,))
+    cursor.execute('SELECT * FROM clients WHERE nom like ?', (post_id,))
     data = cursor.fetchall()
     conn.close()
     # Rendre le template HTML et transmettre les données
     return render_template('read_data.html', data=data)
+
+@app.route('/fiche_client/<int:post_id>')
+def Readfiche(post_id):
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM clients WHERE nom like', (post_id,))
+    data = cursor.fetchall()
+    conn.close()
+    # Rendre le template HTML et transmettre les données
+    return render_template('read_data.html', data=data)
+
 
 @app.route('/consultation/')
 def ReadBDD():
