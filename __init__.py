f8036737ff9406dbbbca85f5b19acef2b5493d9b
cm2025ma-cmyk <cholinmatthieu@gmail.com>
2026-01-07 15:57:36 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -43,7 +43,7 @@ def authentification():
 def Readfiche(post_id):
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM clients WHERE = ?', (post_id,))
+    cursor.execute('SELECT * FROM clients WHERE id = ?', (post_id,))
     data = cursor.fetchall()
     conn.close()
     # Rendre le template HTML et transmettre les données
