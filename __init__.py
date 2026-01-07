c461d1b9be16e9ac08ac11f109f9519e3a460ef3
cm2025ma-cmyk <cholinmatthieu@gmail.com>
2026-01-07 15:34:28 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -39,16 +39,6 @@ def authentification():
 
     return render_template('formulaire_authentification.html', error=False)
 
-@app.route('/fiche_client/<string:nom>')
-def Readfiche(nom):
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM clients WHERE nom = ?', (nom,))
-    data = cursor.fetchall()
-    conn.close()
-    # Rendre le template HTML et transmettre les données
-    return render_template('read_data.html', data=data)
-
 @app.route('/fiche_client/<int:post_id>')
 def Readfiche(post_id):
     conn = sqlite3.connect('database.db')
