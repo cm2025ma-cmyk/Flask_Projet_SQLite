b8753b800e2129b2e358f9840f8f820910d470ca
cm2025ma-cmyk <cholinmatthieu@gmail.com>
2026-02-18 15:12:33 +0100

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -24,6 +24,18 @@ cur.execute("INSERT INTO livres (titre, auteur, stock) VALUES (?, ?, ?)", ('Harr
 cur.execute("INSERT INTO livres (titre, auteur, stock) VALUES (?, ?, ?)", ('Les Misérables', 'Victor Hugo', 2))
 cur.execute("INSERT INTO livres (titre, auteur, stock) VALUES (?, ?, ?)", ('L''Étranger', 'Albert Camus', 0))
 
+# ICI c'est l'appplication WEB task   #
+
+# Tâche 1 : Non terminée (état = 0 ou False)
+cur.execute("INSERT INTO taches (titre, description, date_echeance, etat) VALUES (?, ?, ?, ?)",
+            ('Projet NFP107', 'Faire une app web', '2026-18-02', 0))
+
+# Tâche 2 : Terminée (état = 1 ou True)
+cur.execute("INSERT INTO taches (titre, description, date_echeance, etat) VALUES (?, ?, ?, ?)",
+            ('Apprendre Flask', 'Suivre le tutoriel pour créer le gestionnaire de tâches.', '2025-10-25', 1))
+
 connection.commit()
 connection.close()
 print("Base de données initialisée avec Clients et Livres et Emprunts !")
+print("Base de données initialisée avec task !")
+
